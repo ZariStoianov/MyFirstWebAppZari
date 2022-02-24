fb78d4a3ed1f1d5b5710a3c5423ad53ee7c132d5
babo4 <babo4@DESKTOP-L91TC75>
2022-02-24 19:48:09 +0200

    Add project files.

---
+++ MyFirstWebAppZari/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstWebAppZari.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-L91TC75\\SQLEXPRESS;Database=CarService;Integrated Security=True;");
+            }
+
+        }
+    }
+}
---
+++ MyFirstWebAppZari/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstWebAppZari.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
